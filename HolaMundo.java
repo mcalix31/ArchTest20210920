07b28db834aa62e05ceddb1a5e4b8a43e5e98ad7
mcalix31 <91105779+mcalix31@users.noreply.github.com>
2021-09-20 21:40:04 -0600

    Add files via upload

---
+++ HolaMundo.java
@@ -0,0 +1,31 @@
+/*
+Mario Calix
+ */
+package HolaMundo;
+
+public class HolaMundo {
+    
+    public static void main(String args[])
+    {
+        System.out.println("Hola Mundo en JAVA");
+        
+        var miVariableEntera = 100;    // Variable inicializada en valor 100
+        var miVariableCadena = "Mario Calix"; // Variable tipo cadena de caracteres
+        
+        System.out.println(miVariableEntera);
+        System.out.println(miVariableCadena);
+     
+        miVariableCadena = "Adios, Clase";
+        
+        System.out.println(miVariableCadena);  
+        var i = 100;
+        var j = 20;
+        
+        if ( i > j){
+                System.out.println("Se cumple la comparacion");
+                                       
+    }
+     
+    }
+    
+}
